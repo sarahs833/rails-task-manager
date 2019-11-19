19c711a78cd77cee4d89830d018a165cd9581975
sarah wang <sarahwang47@gmail.com>
2019-11-19 16:22:21 +0800

    rails with active

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Task.create(title: "laundry",details: "do not mix the color", completed: false)
+Task.create(title: "Studying",details: "A lot of flashcard to do ", completed: false)
+Task.create(title: "diner",details: "check the shop list", completed: true)
+
+puts "created #{Task.count}!"
